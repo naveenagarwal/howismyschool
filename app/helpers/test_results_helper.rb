786474a63345b319e96f47362055681f826f8a2d
Naveen Agarwal <naveenagarwal287@gmail.com>
2014-06-30 15:43:49 +0530

    changed default color for bars

--- app/helpers/test_results_helper.rb
+++ app/helpers/test_results_helper.rb
@@ -3,10 +3,12 @@ module TestResultsHelper
   def barchart_for_latest_test_results(container)
     "
       <script type='text/javascript'>
+        var colors = ['#AF0202', '#EC7A00']; //, '#FCD200', '#81C714'];
         var myChart = new JSChart('#{container}', 'bar');
         myChart.setDataJSON('/draw_chart/chart_for_latest_test_results.json?chart_type=bar');
         myChart.setAxisNameX('Tests');
         myChart.setAxisNameY('Percentage(%)', true);
+        myChart.colorizeBars(colors);
         myChart.draw();
       </script>
     ".html_safe
