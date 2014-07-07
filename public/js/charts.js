2d83fa005f878276cd78c42cee4a2821bbd56369
Naveen Agarwal <naveenagarwal287@gmail.com>
2014-07-07 14:51:11 +0530

    charts css

--- public/js/charts.js
+++ public/js/charts.js
@@ -3,46 +3,53 @@ var colors = ['#AF0202', '#EC7A00', '#FCD200', '#81C714'];
 function Charts(){
 }
 
-Charts.drawLatestTestResultsChart = function(container){
+Charts.drawChart = function(url, container, xlabel, ylabel){
   var myChart = new JSChart(container, 'bar');
-  myChart.setDataJSON('/draw_chart/chart_for_latest_test_results.json?chart_type=bar');
-  myChart.setAxisNameX('Tests');
-  myChart.setAxisNameY('Percentage(%)', true);
+  myChart.setDataJSON(url);
+  myChart.setAxisNameX(xlabel);
+  myChart.setAxisNameY(ylabel, true);
+  myChart.setAxisNameColor("#000000");
+  myChart.setAxisValuesColor("#000000");
+  myChart.setBarValuesColor("#000000");
   myChart.colorizeBars(colors);
-  myChart.setFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.setLabelFontFamily('"Comic Sans MS", cursive, sans-serif');
   myChart.draw();
+  myChart.resize(500, 350)
+}
+
+Charts.drawLatestTestResultsChart = function(container){
+  var url       = '/draw_chart/chart_for_latest_test_results.json?chart_type=bar',
+      container = container,
+      xlabel    = 'Tests',
+      ylabel    = 'Percentage(%)';
+
+  Charts.drawChart(url, container, xlabel, ylabel);
 }
 
 Charts.drawClassRoomChart = function(class_room, container){
-  var myChart = new JSChart(container, 'bar');
-  myChart.setDataJSON('/draw_chart/' + class_room + '/chart_for_class_room.json?chart_type=bar');
-  myChart.setAxisNameX('Tests');
-  myChart.setAxisNameY('Percentage(%)', true);
-  myChart.colorizeBars(colors);
-  myChart.setFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.setLabelFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.draw();
+  var url       = '/draw_chart/' + class_room + '/chart_for_class_room.json?chart_type=bar',
+      container = container,
+      xlabel    = 'Tests',
+      ylabel    = 'Percentage(%)';
+
+  Charts.drawChart(url, container, xlabel, ylabel);
+
 }
 
 Charts.drawClassRoomToppersSubjectwiseChart = function(class_room, container){
-  var myChart = new JSChart(container, 'bar');
-  myChart.setDataJSON('/draw_chart/' + class_room + '/chart_for_class_room_toppers_subjectwise.json?chart_type=bar');
-  myChart.setAxisNameX('Student/Subejct/Test');
-  myChart.setAxisNameY('Percentage(%)', true);
-  myChart.colorizeBars(colors);
-  myChart.setFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.setLabelFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.draw();
+  var url       = '/draw_chart/' + class_room + '/chart_for_class_room_toppers_subjectwise.json?chart_type=bar',
+      container = container,
+      xlabel    = 'Student/Subejct/Test',
+      ylabel    = 'Percentage(%)';
+
+  Charts.drawChart(url, container, xlabel, ylabel);
+
 }
 
 Charts.drawClassRoomToppersTestwiseChart = function(class_room, container){
-  var myChart = new JSChart(container, 'bar');
-  myChart.setDataJSON('/draw_chart/' + class_room + '/chart_for_class_room_toppers_testwise.json?chart_type=bar');
-  myChart.setAxisNameX('Student/Test');
-  myChart.setAxisNameY('Percentage(%)', true);
-  myChart.colorizeBars(colors);
-  myChart.setFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.setLabelFontFamily('"Comic Sans MS", cursive, sans-serif');
-  myChart.draw();
+  var url       = '/draw_chart/' + class_room + '/chart_for_class_room_toppers_testwise.json?chart_type=bar',
+      container = container,
+      xlabel    = 'Student/Test',
+      ylabel    = 'Percentage(%)';
+
+  Charts.drawChart(url, container, xlabel, ylabel);
 }
