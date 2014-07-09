513d21a041fd9e94f0cd6f57773b1f45dd4baf31
Naveen Agarwal <naveenagarwal287@gmail.com>
2014-07-09 15:30:51 +0530

    search modified

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -27,24 +27,24 @@ class SearchController < ApplicationController
     respond_to do |format|
       format.js {
         begin
-          # search = current_teacher.searches.where(text: params[:q]).first || current_teacher.searches.new
-          # search.searcher = current_teacher
-          # search.text = params[:q]
+          search = current_teacher.searches.where(text: params[:q]).first || current_teacher.searches.new
+          search.text = params[:q]
+          search.school_branch_id = current_school_branch.id
+          search.save
 
           @query = Sunspot.search [Student, ClassTest] do
             data_accessor_for(Student).include = [:class_room]
             data_accessor_for(ClassTest).include = [:test_results]
 
-            fulltext params[:q]
+            fulltext params[:q] do
+              minimum_match 1
+            end
+
             order_by :score, :desc
             with :school_branch_id, current_school_branch.id
           end
 
           @results = @query.results
-          # search.result << @results
-          # search.result.uniq!
-          # search.save
-
         rescue Exception => e
           byebug
           @results = []
